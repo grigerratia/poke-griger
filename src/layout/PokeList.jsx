a5f8a732b0a81fff92be57a89760c738b088969c
Griger Ratia <grigerratia@gmail.com>
2024-04-17 18:28:38 -0400

    Fix llamado de imágenes en Buscador

--- src/layout/PokeList.jsx
+++ src/layout/PokeList.jsx
@@ -9,23 +9,42 @@ import "../styles/PokeList.css";
 const PokeList = () => {
 	const context = useContext(MiContexto);
 	const { filteredList, listPokemons, setListPokemons, lengthBatch, setLengthBatch } = context;
+	const [firstNum, setFirstNum] = useState([1, 2, 3, 4, 5, 6])
+	const [count, setCount] = useState(1)
 
 
 
 
-	//siguiente y anterior lote de la lista de pokemons
+	//siguiente y anterior lote de la lista de pokemons por flechas
 	const navBatch = (op) => {
 		if (op === "next") {
 			setLengthBatch({
 				initBatch: lengthBatch.initBatch + 12,
 				endBatch: lengthBatch.endBatch + 12
 			})
+			setCount(count + 1)
 		} else {
 			if (lengthBatch.initBatch === 1) return
 			setLengthBatch({
 				initBatch: lengthBatch.initBatch - 12,
 				endBatch: lengthBatch.endBatch - 12
 			})
+			setCount(count - 1)
+		}
+	}
+
+	//siguiente y anterior lote de la lista de pokemons por números
+	const navNum = (num) => {
+		if (num === count) return
+		setCount(num)
+		console.log(num);
+		setLengthBatch({
+			initBatch: (12 * num),
+			endBatch: (12 * num) + 11
+		})
+
+		if (num === firstNum.at(-1)) {
+			setFirstNum(firstNum.map(num => num++))
 		}
 	}
 
@@ -54,9 +73,11 @@ const PokeList = () => {
 				{
 					lengthBatch.initBatch != 1 && <div onClick={() => navBatch("before")}>{"<<"}</div>
 				}
-				<div>2</div>
-				<div>3</div>
-				<div>4</div>
+				{
+					firstNum.map(el => (
+						<div key={el} onClick={() => navNum(el)}>{el}</div>
+					))
+				}
 				<div onClick={() => navBatch("next")}>{">>"}</div>
 			</div>
 		</div>
