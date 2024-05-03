786410ce83699d80fa0fc5cfeeac40d1077ba6b2
Griger Ratia <grigerratia@gmail.com>
2024-05-03 17:13:14 -0400

    HU Ordenar la lista de 4 maneras distintas

--- src/Components/Ordenador.jsx
+++ src/Components/Ordenador.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { getIdOfUrl } from '../utils/getIdOfUrl'
 import "../styles/Ordenador.css";
 import { MiContexto } from "../context/context";
 
@@ -7,31 +8,72 @@ const Ordenador = () => {
 	const context = useContext(MiContexto);
 	const { setFilteredList, filteredList, allPokemons } = context
 
-
+	//ORDENA DESDE EL NÚMERO MENOR AL MAYOR
 	const orderSup = () => {
+		if (filteredList === false) {
+			setFilteredList([...allPokemons].sort((a, b) => {
+				if (getIdOfUrl(a.url) < getIdOfUrl(b.url)) { return -1; }
+				if (getIdOfUrl(a.url) > getIdOfUrl(b.url)) { return 1; }
+				return 0;
+			}))
+		}
+		setFilteredList([...filteredList].sort((a, b) => {
+			if (getIdOfUrl(a.url) < getIdOfUrl(b.url)) { return -1; }
+			if (getIdOfUrl(a.url) > getIdOfUrl(b.url)) { return 1; }
+			return 0;
+		}))
 	}
-	const orderInf = () => {
 
+	//ORDENA DESDE EL MAYOR MENOR AL MENOR
+	const orderInf = () => {
+		if (filteredList === false) {
+			setFilteredList([...allPokemons].sort((a, b) => {
+				if (getIdOfUrl(a.url) < getIdOfUrl(b.url)) { return 1; }
+				if (getIdOfUrl(a.url) > getIdOfUrl(b.url)) { return -1; }
+				return 0;
+			}))
+		}
+		setFilteredList([...filteredList].sort((a, b) => {
+			if (getIdOfUrl(a.url) < getIdOfUrl(b.url)) { return 1; }
+			if (getIdOfUrl(a.url) > getIdOfUrl(b.url)) { return -1; }
+			return 0;
+		}))
 	}
+
+	//ORDENA DESDE ALFABETICAMENTE DESDE LA A HASTA LA Z
 	const orderAZ = () => {
 		if (filteredList === false) {
 			setFilteredList([...allPokemons].sort((a, b) => a.name.localeCompare(b.name)))
 			return
 		}
-		setFilteredList(filteredList.sort((a, b) => a.name.localeCompare(b.name)))
+		setFilteredList([...filteredList].sort((a, b) => a.name.localeCompare(b.name)))
 	}
-	const orderZA = () => {
 
+	//ORDENA DESDE ALFABETICAMENTE DESDE LA Z HASTA LA A
+	const orderZA = () => {
+		if (filteredList === false) {
+			setFilteredList([...allPokemons].sort((a, b) => {
+				if (a.name < b.name) { return 1; }
+				if (a.name > b.name) { return -1; }
+				return 0;
+			}))
+			return
+		}
+		setFilteredList([...filteredList].sort((a, b) => {
+			if (a.name < b.name) { return 1; }
+			if (a.name > b.name) { return -1; }
+			return 0;
+		}))
 	}
 
 	return (
 		<div className='ordenador'>
 			<div className='ordenar'>Ordenar</div>
 			<ul>
-				<li value='numero-superior'>Número superior</li>
-				<li value='numero-inferior'>Número inferior</li>
-				<li value='a-z' onClick={orderAZ}>A - Z</li>
-				<li value='z-a'>Z - A</li>
+				<li onClick={orderSup}>Número superior</li>
+				<li onClick={orderInf}>Número inferior</li>
+				<li onClick={orderAZ}>A - Z</li>
+				<li onClick={orderZA}>Z - A</li>
 			</ul>
 		</div>
 	);
