3ff7f86881bdd7008509b39e749ccdfe7ca7a1a0
Griger <grigerratia@gmail.com>
2024-08-17 08:01:13 -0400

    T: Adelanto del proyecto

--- src/Components/RenderTipos.jsx
+++ src/Components/RenderTipos.jsx
@@ -1,21 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/RenderTipos.css";
 
 const RenderTipos = () => {
+
+	const typeRef = useRef(null)
+
 	let [isTipos, setIsTipos] = useState(null);
-	let [estate, setState] = useState(false)
+	let [active, setActive] = useState(false)
 	useEffect(() => {
 		fetch("https://pokeapi.co/api/v2/type/")
 			.then((res) => res.json())
 			.then((data) => {
 				setIsTipos(data.results);
 			});
-	}, []);
+	}, [active]);
 
+	function hacerAlgo(nameOfClass){
+		// alert(nameOfClass)
+
+		const thisClass = nameOfClass
+		const renderTipos = document.querySelector(thisClass)
+		// renderTipos.classList.add("renderTActive")
+		typeRef.current.classList.add("renderTActive")
+		setActive(true)
+
+	}
+	
 	return (
-		<div className='renderT'>
+		<div className='renderT' >
 			{isTipos?.map((el) => (
-				<span key={el.name} className={el.name}>
+				<span
+					key={el.name}
+					className={el.name}
+					ref={typeRef}
+					onClick={(event) => {
+						event.stopPropagation();
+						hacerAlgo(el.name);
+					}}>
 					{el.name}
 				</span>
 			))}
